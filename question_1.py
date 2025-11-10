9cc117e76c630f958ee318427ac29ee29e88e58a
Yevhen <116503743+mlioonn@users.noreply.github.com>
2025-11-10 17:14:35 +0000

    Update question_1.py

--- question_1.py
+++ question_1.py
@@ -1,3 +1,20 @@
+"""
+Functions and Strings
+All code for this question must be written in the file question_1.py provided.
+Write a function seconds_to_time(time) that takes an int as a parameter representing
+the time in seconds, and returns a string (str type). The format of the returned String is as
+follows:
+(a) [8 marks] hh:mm:ss where two digits are always used for hours (h), minutes (m), and
+seconds (s) when the time is greater than an hour. For example 3625 seconds is
+represented by 01:00:25.
+(b) [8 marks] mm:ss where two digits are always used for minutes (m), and seconds (s)
+when the time is less than an hour. For examples: 85 seconds is represented by 01:25,
+and 16 seconds is represented by 00:16.
+(c) [4 marks] In addition the method returns None if the time is negative or greater than
+99:59:59
+"""
+
+
 hours = 0
 minutes = 0
 seconds = 0
