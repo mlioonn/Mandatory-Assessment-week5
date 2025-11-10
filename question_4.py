5823adb741196dc580a41a5d4e4706a400a20398
Yevhen <116503743+mlioonn@users.noreply.github.com>
2025-11-10 17:18:56 +0000

    Update question_4.py
    question is not fully right.

--- question_4.py
+++ question_4.py
@@ -1,3 +1,25 @@
+"""
+Built-in Data Structures.
+All code for this question must be written in the file question_4.py.
+The Caesar cipher is a very simple encryption method and it is easily breakable. In this question
+we use an improved encryption method. Given a plain text message T of n characters using an
+alphabet A of m letters, and a list S of n positive integers from the set {0, 2, .., k − 1}, T is
+encrypted by shifting the kth character T[k] by an amount S[k]. An example is given in Figure 1
+where T = [B, A, B, Y] and S = [2, 1, 1, 3]. The first character ’B’ is shifted by 2 giving the
+character ’D’, then the second character ’A’ is shifted by 1 giving ’B’ and so on. The encrypted
+message is "DBCB".
+
+Implement the function encrypt(message, shifts, alphabet) that takes a plain
+text message as a string, the shifts sequence as a list of integers, and the alphabet as
+a string. The alphabet can be any sequence of symbols, not necessarily the English alphabet.
+The function returns a string containing the encrypted message.
+The function returns None if:
+• the message contains characters that are not in the alphabet,
+• the size of the shifts is not the same as the size of message,
+• shifts contains negatives values or values greater or equal to the size of the alphabet
+"""
+
+
 def encrypt(message, shifts, alphabet):
     string = ""
     wordList = list(message)
